aef433dc0dcc1d259bf9b2bc1fe767c6e5f47cab
Vinícius Castelo <vkstello@gmail.com>
2022-11-16 09:13:59 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -13,6 +15,8 @@ public class Program {
 
         Seller seller = new Seller(1, "Bob", "bob@gmail.com", new Date(), 3000.00, obj);
         System.out.println(seller);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
     }
 }
